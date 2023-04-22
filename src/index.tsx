35595913f70e34da93068b0a3bc6030eec038fdd
YungMilky <filtersome@gmail.com>
2023-04-22 09:50:22 +0200

    privacy policy for promptly

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import PromptlyPrivacyPolicy from './Reusables/Promptly/PromptlyPrivacyPolicy';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}></Route>
+        <Route
+          path='/promptlyPrivacyPolicy'
+          element={<PromptlyPrivacyPolicy />}
+        ></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
